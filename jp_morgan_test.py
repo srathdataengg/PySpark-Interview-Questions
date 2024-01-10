97ee2950fdd8579bb70912e0ad6fcdcffefa305c
srathdataengg <srathdataengineer@gmail.com>
2024-01-11 02:37:30 +0530

    jp morgan test

---
+++ jp_morgan_test.py
@@ -0,0 +1,28 @@
+"""
+id,name,salary
+101,"rahul",1000
+201,"sachin",1500
+301,"saurav",1000
+
+
+id,name,salary - csv
+
+id,address - json
+101,"101 address"
+201,"201 address"
+301,"301 address"
+
+# find out the address of id which salary is highest
+# drop the null values from below salary table
+# check palindrome number/string
+# all people salary with less than 1000 override them with salary + salary * 30
+
+salary
+1000
+1000
+1500
+8000
+10000
+600
+
+"""
