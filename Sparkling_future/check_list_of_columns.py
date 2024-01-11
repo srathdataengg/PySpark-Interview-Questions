91d47fb86a67d164dc190df4dff9bb9a5dc763e0
srathdataengg <srathdataengineer@gmail.com>
2024-01-11 15:01:19 +0530

    Sparkling question

---
+++ Sparkling_future/check_list_of_columns.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("check list of columns").getOrCreate()
+
+df = spark.read.csv("/Users/soumyakantarath/Desktop/PySpark-Interview-Questions/datasets/Orders.csv", header=True)
+
+df.show(5)
+
+list1 = ['SALES', 'Profit']
+
+df_cols_list = df.columns
+lowerlist1 = (i.lower() for i in list1)
+
+# Python all function checks if all elements are True then true otherwise false
+# python list operations allow to write multiple expression in a list Ex:- res =[i.upper() for i in test_list]
+
+if all([i in df_cols_list for i in lowerlist1]):
+    print("True")
+else:
+    print("false")
